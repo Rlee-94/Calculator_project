d42a0aac8eb3b524d776fab75667db4c1bafc45e
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-10-08 12:06:30 -0500

    Added AC/DELETE button
    -Added buttons to the top of the Calculator

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const screenDisplay = document.querySelector(".calc__screen");
 const numberBtns = document.querySelectorAll(".calc__button");
+const add = document.getElementById("add");
+const substract = document.getElementById("subtract");
+const multiply = document.getElementById("multiply");
+const divide = document.getElementById("divide");
 
 numberBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     screenDisplay.textContent = screenDisplay.textContent + btn.textContent;
-    console.log(btn.textContent);
   });
 });
+
+function addNum(x, y) {
+  return x + y;
+}
+
+function substractNum(x, y) {
+  return x - y;
+}
+
+function multiplyNum(x, y) {
+  return x * y;
+}
+
+function divideNum(x, y) {
+  return x / y;
+}
