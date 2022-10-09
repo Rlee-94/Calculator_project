c82edd88918936353ab997fdadc0dfb26386a534
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-10-09 14:23:24 -0500

    Added button hover/more JS logic for calculations.
    -Added a transform hover effect for buttons and swapped equals/add btn.
    -Changed bg color of all operators to a blue color to identify.
    -Added slight JS logic by creating a switch statement to run operate
    function.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const screenDisplay = document.querySelector(".calc__screen");
-const numberBtns = document.querySelectorAll(".calc__button");
-const add = document.getElementById("add");
-const substract = document.getElementById("subtract");
-const multiply = document.getElementById("multiply");
-const divide = document.getElementById("divide");
+const numberBtns = document.querySelectorAll("[data-number]");
+const operatorBtns = document.querySelectorAll("[data-operator]");
+const equalsButton = document.getElementById("equals");
+const allClearButton = document.getElementById("all-clear");
+const deleteButton = document.getElementById("delete");
+const pointButton = document.getElementById("decimal");
 
 numberBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -11,18 +12,36 @@ numberBtns.forEach((btn) => {
   });
 });
 
-function addNum(x, y) {
+function add(x, y) {
   return x + y;
 }
 
-function substractNum(x, y) {
+function subtract(x, y) {
   return x - y;
 }
 
-function multiplyNum(x, y) {
+function multiply(x, y) {
   return x * y;
 }
 
-function divideNum(x, y) {
+function divide(x, y) {
   return x / y;
+}
+
+function operate(operator, x, y) {
+  x = Number(x);
+  y = Number(y);
+  switch (operator) {
+    case "+":
+      return add(x, y);
+    case "-":
+      return substract(x, y);
+    case "x":
+      return multiply(a, b);
+    case "÷":
+      if (y === 0) return null;
+      else return divide(x, y);
+    default:
+      return null;
+  }
 }
